4fef3169adbb36ba1cb4461d6524f62a26d5acfb
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-02-01 12:56:22 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// recover of createApp method
+const { createApp } = Vue;
+
+// app creation
+const app = createApp({
+  data() {
+    return {};
+  },
+
+  methods: {},
+});
+
+// running the app
+app.mount("#app");
